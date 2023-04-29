7734f075cf040692d0fb3576d73de4d43f148a5a
Dipesh Shrestha <ypierceineyebydiamond60@gmail.com>
2023-04-29 09:54:24 -0400

    added mongo

--- index.js
+++ index.js
@@ -3,6 +3,51 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// importing mongoclient
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://dipesh:shrestha@shresthacluster.gvlddte.mongodb.net/?retryWrites=true&w=majority";
+// let data = []
+
+
+// Update Data.json file with updated data from MongoDB
+async function modifyData(products){
+    console.log("----Modifying Data.json file:----");
+    console.log(products);
+    fs.writeFile('./public/assets/data.json',JSON.stringify(products),(err)=>{
+        console.log(err);
+    })
+    console.log("----Modifying Data.json successful:----");
+}
+
+//Listing all data from mongoDB
+async function accessAllData(client){
+    
+    console.log("----Accessing all the products:----");
+    const cursor = client.db("lucidmarketplace").collection("productcollection").find({});
+    const products = await cursor.toArray();
+    // console.log(products);
+    console.log("----Accessing all the products successful:----");
+    // console.log(JSON.parse(products));
+    await modifyData(products);
+}
+
+// Connecting to MongoDB
+async function connectToMongoDB(){
+    const client = new MongoClient(uri);
+    try{
+        client.connect();
+        console.log("----Connection is successful.----");
+        //Reading all the data from MongoDB (productcollection)
+        await accessAllData(client);
+    }catch(e){
+        console.log("----Error occured while establishing connection----",e);
+
+    }finally{
+        client.close();
+        console.log("----Closing the connection----")
+    }
+}
+
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
         fs.readFile(path.join(__dirname,'public','index.html'),(err, content)=>{
@@ -39,6 +84,9 @@ const server = http.createServer((req,res)=>{
         filestream.pipe(res);
     }
     else if(req.url === '/api'){
+        //Connecting to MongoDB
+        connectToMongoDB();
+        
         fs.readFile(path.join(__dirname,'public/assets','data.json'),(err,content)=>{
             if(err) throw err;
             res.setHeader('Access-Control-Allow-Origin', '*');
